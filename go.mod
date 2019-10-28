e6a4bcdedcdfdded06fe55886222e55c4a0e905f
cclehui <you@example.com>
2019-10-28 20:24:04 +0800

    add tcp server 01

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/cclehui/server_on_gnet
 
 go 1.13
 
-require github.com/panjf2000/gnet v1.0.0-beta.8 // indirect
+require (
+	github.com/panjf2000/ants v1.3.0 // indirect
+	github.com/panjf2000/gnet v1.0.0-beta.8 // indirect
+)
--- tcp_fixed_head_server.go
+++ tcp_fixed_head_server.go
@@ -1,19 +1,38 @@
 package main
 
 import (
+	"errors"
 	"flag"
 	"fmt"
 	"log"
+	"time"
 
+	"github.com/panjf2000/ants"
 	"github.com/panjf2000/gnet"
 )
 
 //网络编程
 //tcp server 简单的 固定头部长度协议
 
+const DefaultAntsPoolSize = 1024 * 1024
+
+func NewTCPFixHeadServer(port int) *TCPFixHeadServer {
+
+	options := ants.Options{ExpiryDuration: time.Second * 10, Nonblocking: true}
+	defaultAntsPool, _ := ants.NewPool(DefaultAntsPoolSize, ants.WithOptions(options))
+
+	server := &TCPFixHeadServer{}
+
+	server.Port = port
+	server.WorkerPool = defaultAntsPool
+
+	return server
+}
+
 type TCPFixHeadServer struct {
 	*gnet.EventServer
-	Port int
+	Port       int
+	WorkerPool *ants.Pool
 }
 
 func (tcpfhs *TCPFixHeadServer) OnInitComplete(srv gnet.Server) (action gnet.Action) {
@@ -23,8 +42,36 @@ func (tcpfhs *TCPFixHeadServer) OnInitComplete(srv gnet.Server) (action gnet.Act
 }
 func (tcpfhs *TCPFixHeadServer) React(c gnet.Conn) (out []byte, action gnet.Action) {
 	out = c.Read()
-	c.ResetBuffer()
+
+	tcpfhs.WorkerPool.Submit(func() {
+		protocal := &TCPFixHeadProtocal{Conn: c}
+		protocal.decode(out)
+	})
 	return
+}
+
+type ProtocalData struct {
+	Type       int8
+	DataLength int
+	data       []byte
+}
+
+type TCPFixHeadProtocal struct {
+	Conn    gnet.Conn
+	Handler func(pdata *ProtocalData)
+}
+
+func (tcpfhp *TCPFixHeadProtocal) decode(data []byte) (*ProtocalData, error) {
+	if len(data) < 5 {
+		return nil, errors.New("data not full")
+	}
+
+	fmt.Println(time.Now().Format("2006-01-02 15:04:05"), "\t", string(data))
+
+	//if 如果是整个解析完成
+	//tcpfhp.Conn.ResetBuffer()
+
+	return nil, nil
 }
 
 type TCPFixHeadClient struct {
@@ -41,8 +88,7 @@ func main() {
 	flag.BoolVar(&multicore, "multicore", true, "multicore")
 	flag.Parse()
 
-	tcpServer := new(TCPFixHeadServer)
-	tcpServer.Port = port
+	tcpServer := NewTCPFixHeadServer(port)
 
 	log.Fatal(gnet.Serve(tcpServer, fmt.Sprintf("tcp://:%d", port), gnet.WithMulticore(multicore)))
 
