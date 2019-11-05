ee91d3af2fa34942ad30a6c2d11f951c6b752598
cclehui <you@example.com>
2019-11-05 22:06:49 +0800

    固定头部协议 02

--- tcp_fixed_head_server.go
+++ tcp_fixed_head_server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"encoding/binary"
 	"errors"
 	"flag"
@@ -48,14 +49,19 @@ func (tcpfhs *TCPFixHeadServer) OnInitComplete(srv gnet.Server) (action gnet.Act
 func (tcpfhs *TCPFixHeadServer) React(c gnet.Conn) (out []byte, action gnet.Action) {
 	fmt.Println("rrrrrrrrrrrr")
 
+	//在 reactor 协程中做解码操作
+	protocal := &TCPFixHeadProtocal{HeadLength: DefaultHeadLength, Conn: c}
+	protocalData, err := protocal.decode()
+	if err != nil {
+		log.Printf("React WorkerPool Handler error :%v\n", err)
+	}
+
 	tcpfhs.WorkerPool.Submit(func() {
-		protocal := &TCPFixHeadProtocal{HeadLength: DefaultHeadLength, Conn: c}
-
-		if protocalData, err := protocal.decode(); protocalData != nil {
-
-			fmt.Printf("完整协议数据1111, %v, error:%v\n", protocalData, err)
-
+		//具体业务在 worker pool中处理
+		if protocalData != nil {
+			fmt.Printf("完整协议数据1111, %v, data:%s\n", protocalData, string(protocalData.Data))
 		}
+
 	})
 	return
 }
@@ -64,6 +70,9 @@ type ProtocalData struct {
 	Type       uint16
 	DataLength uint32
 	Data       []byte
+
+	//headDecode bool
+	//Lock       sync.Mutex
 }
 
 type TCPFixHeadProtocal struct {
@@ -81,86 +90,69 @@ func (tcpfhp *TCPFixHeadProtocal) decode() (*ProtocalData, error) {
 		//解析协议 header
 		if tempSize, headData := tcpfhp.Conn.ReadN(tcpfhp.HeadLength); tempSize == tcpfhp.HeadLength {
 
-			fmt.Println("hhhhhhhhh,", headData)
-
 			newConContext := ProtocalData{}
 			//数据长度
-			/*
-				var length uint32
-				lengthBytesBuffer := bytes.NewBuffer(headData[1:tcpfhp.HeadLength])
-				err := binary.Read(lengthBytesBuffer, binary.BigEndian, &length)
-				if err != nil {
-					fmt.Println("eeee11111,", err)
-					return nil, err
-				}
-				newConContext.DataLength = length
-			*/
-
-			newConContext.DataLength = binary.BigEndian.Uint32(headData[2:tcpfhp.HeadLength])
-
-			//数据类型
-			/*
-				var dataType int8
-				typeBytesBuffer := bytes.NewReader(headData[0:1])
-				err2 := binary.Read(typeBytesBuffer, binary.BigEndian, &dataType)
-				if err2 != nil {
-					return nil, err2
-				}
-				newConContext.Type = dataType
-			*/
-			newConContext.Type = binary.BigEndian.Uint16(headData[0:2])
-
-			fmt.Println("hhhhhhhhh, 2222222222,", newConContext)
+			bytesBuffer := bytes.NewBuffer(headData)
+			binary.Read(bytesBuffer, binary.BigEndian, &newConContext.Type)
+			binary.Read(bytesBuffer, binary.BigEndian, &newConContext.DataLength)
 
 			tcpfhp.Conn.SetContext(newConContext)
 
-			return nil, nil
-
 		} else {
 			return nil, nil
-		}
-
-	} else {
-		//解析协议数据
-		if protocalData, ok := curConContext.(ProtocalData); !ok {
-			tcpfhp.Conn.SetContext(nil)
-			return nil, errors.New("context 数据异常")
-
-		} else {
-			dataLength := int(protocalData.DataLength)
-
-			if dataLength < 1 {
-				return &protocalData, nil
-			}
-
-			if tempSize, data := tcpfhp.Conn.ReadN(dataLength); tempSize == dataLength {
-				protocalData.Data = data
-
-				return &protocalData, nil
-
-			} else {
-				return nil, nil
-			}
 		}
 	}
+
+	//解析协议数据
+	if protocalData, ok := tcpfhp.Conn.Context().(ProtocalData); !ok {
+		tcpfhp.Conn.SetContext(nil)
+		return nil, errors.New("context 数据异常")
+
+	} else {
+		dataLength := int(protocalData.DataLength)
+
+		if dataLength < 1 {
+			tcpfhp.Conn.SetContext(nil)
+			return &protocalData, nil
+		}
+
+		if tempSize, data := tcpfhp.Conn.ReadN(dataLength); tempSize == dataLength {
+			protocalData.Data = data
+
+			tcpfhp.Conn.SetContext(nil)
+			return &protocalData, nil
+
+		} else {
+			return nil, nil
+		}
+	}
+
 	return nil, nil
 }
 
 //output 数据编码
-func (tcpfhp *TCPFixHeadProtocal) encode(output string) ([]byte, error) {
+func (tcpfhp *TCPFixHeadProtocal) encodeWrite(output []byte, conn net.Conn) error {
 
 	var dataLength uint32 = uint32(len(output))
 
 	var dataType uint16 = 0
 
+	fmt.Println("发出的数据:", output)
+
 	//result := []byte(fmt.Sprintf("0%d%s", dataLength, output))
 
-	result := make([]byte, tcpfhp.HeadLength)
-	binary.BigEndian.PutUint16(result[0:2], dataType)
-	binary.BigEndian.PutUint32(result[2:tcpfhp.HeadLength], dataLength)
-	result = append(result, []byte(output)...)
+	fmt.Println("encodeWrite,", binary.Write(conn, binary.BigEndian, &dataType))
+	binary.Write(conn, binary.BigEndian, &dataLength)
+	binary.Write(conn, binary.BigEndian, &output)
 
-	return result, nil
+	/*
+		result := make([]byte, tcpfhp.HeadLength)
+		binary.BigEndian.PutUint16(result[0:2], dataType)
+		binary.BigEndian.PutUint32(result[2:tcpfhp.HeadLength], dataLength)
+		result = append(result, []byte(output)...)
+	*/
+
+	return nil
 }
 
 type TCPFixHeadClient struct {
@@ -200,19 +192,29 @@ func tcpFHTestClient(port int) {
 		log.Printf("tcpFHTestClient, Dail error:%v\n", err)
 	}
 
+	//badData := []byte("xxx")
+
 	for i := 1; i <= 10; i++ {
+		//for i := 1; i <= 2; i++ {
 		data := strings.Repeat(strconv.Itoa(i), i)
+		data = data + "abc"
+
+		/*
+			if i == 2 {
+				err2 := binary.Write(conn, binary.BigEndian, badData)
+				fmt.Println("发送干扰数据, ", err2)
+			}
+		*/
 
 		protocal := &TCPFixHeadProtocal{HeadLength: DefaultHeadLength}
 
-		if dataEncoded, err2 := protocal.encode(data); err2 == nil {
-			fmt.Println(string(dataEncoded))
-			conn.Write(dataEncoded)
-		}
+		protocal.encodeWrite([]byte(data), conn)
 
 		fmt.Println(data)
 
 		time.Sleep(time.Second * 1)
 	}
+
+	time.Sleep(time.Second * 86400)
 
 }
