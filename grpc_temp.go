e7d01152c8713eaf945cbbc80e1c1aa23ce07958
cclehui <763414242@qq.com>
2019-12-08 20:58:42 +0800

    grpc 02 client

--- grpc_temp.go
+++ grpc_temp.go
@@ -9,6 +9,7 @@ import (
 
 	"github.com/cclehui/server_on_gnet/protobuf"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/keepalive"
 )
 
 const (
@@ -43,11 +44,25 @@ func startGrpcServer() {
 
 }
 
+var kacp = keepalive.ClientParameters{
+	Time:                10 * time.Second, // send pings every 10 seconds if there is no activity
+	Timeout:             time.Second,      // wait 1 second for ping ack before considering the connection dead
+	PermitWithoutStream: true,             // send pings even without active streams
+}
+
 func clientTest(count int) {
 
-	serverAddress := fmt.Sprintf("localhost%v", port)
+	//serverAddress := fmt.Sprintf("localhost%v", port)
+	serverAddress := fmt.Sprintf("172.16.9.216%v", port)
 
-	conn, err := grpc.Dial(serverAddress, grpc.WithInsecure())
+	/*
+		keepaliveParam := keepalive.ClientParameters{}
+		keepaliveParam.Time = time.Second * 10
+		keepaliveParam.Timeout = time.Second * 2
+		keepaliveParam.PermitWithoutStream = true
+	*/
+
+	conn, err := grpc.Dial(serverAddress, grpc.WithInsecure(), grpc.WithKeepaliveParams(kacp))
 	if err != nil {
 		log.Printf("连接服务端失败, %v\n", err)
 		return
@@ -56,20 +71,27 @@ func clientTest(count int) {
 	log.Printf("连接服务端成功, %v\n", conn.Target())
 	defer conn.Close()
 
-	name := fmt.Sprintf("cclehui_%d", count)
+	for i := 1; i < 4; i++ {
+		go func(num int) {
+			name := fmt.Sprintf("cclehui_%d", num)
+			for {
+				client := protobuf.NewGreeterClient(conn)
 
-	client := protobuf.NewGreeterClient(conn)
+				request := &protobuf.HelloRequest{Name: name}
 
-	request := &protobuf.HelloRequest{Name: name}
+				reply, err2 := client.SayHello(context.Background(), request)
 
-	reply, err2 := client.SayHello(context.Background(), request)
+				if err2 != nil {
+					log.Printf("client:%d, 调用rpc方法失败, %v\n", i, err2)
+					return
+				}
 
-	if err2 != nil {
-		log.Printf("调用rpc方法失败, %v\n", err2)
-		return
+				log.Printf("client:%d, 调用rpc方法成功, server reply:%v\n", i, reply.GetMessage())
+				time.Sleep(time.Second * 1)
+			}
+		}(i)
 	}
-
-	log.Printf("调用rpc方法成功, server reply:%v\n", reply.GetMessage())
+	select {}
 
 	return
 }
@@ -78,16 +100,18 @@ func main() {
 
 	//启动 grpc server
 	go func() {
-		startGrpcServer()
+		//startGrpcServer()
 	}()
 
 	//client test
 	count := 1
 
 	for {
-		time.Sleep(time.Second * 1)
 
 		clientTest(count)
+
+		time.Sleep(time.Second * 1000)
+		time.Sleep(time.Second * 1)
 
 		count++
 	}
